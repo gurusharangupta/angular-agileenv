dce292dfc0a2af8455d4e1dd1bb6955c2feb50c9
gurusharangupta <gupta.gurusharan@gmail.com>
2019-08-08 12:00:23 +0530

    Update header.component.ts

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../service/auth.service';
 import { Router } from '@angular/router';
+import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-header',
@@ -9,15 +10,30 @@ import { Router } from '@angular/router';
 })
 export class HeaderComponent implements OnInit {
 
+  userLoggedIn = false;
+
   constructor(private authService: AuthService, private router: Router) { }
 
   ngOnInit() {
+    if (!!this.authService.user.getValue()) {
+
+      this.router.navigate(['/projects']);
+    }
+
+    this.authService.user.subscribe(
+      user => {
+        if (!!user) { this.userLoggedIn = true; }
+        else { this.userLoggedIn = false }
+
+      });
+
   }
 
   logout() {
 
     this.authService.logout();
     this.router.navigate(['/auth']);
+    
 
   }
 }
