7bcff8a19fb1252ad123bac0b388255998dfb546
gurusharangupta <gupta.gurusharan@gmail.com>
2019-08-26 10:35:31 +0530

    2 files modified

--- src/server.js
+++ src/server.js
@@ -8,47 +8,41 @@ var Kafka = require('no-kafka');
 io.on('connection', (socket) => {
   console.log('USER CONNECTED');
 
-  socket.on('disconnect', function(){
+  socket.on('disconnect', function () {
     console.log('USER DISCONNECTED');
   });
- 
-});
 
-$(function(){
-alert('Hello');
 });
 
 
 http.listen(8091, () => {
   console.log('started on port 8091');
   var consumer = new Kafka.SimpleConsumer({
-        connectionString: 'http://localhost:9092',
-        clientId: 'no-kafka-client'
-    }); 
- 
-// data handler function can return a Promise 
-	var dataHandler = function (messageSet, topic, partition) {
-		messageSet.forEach(function (m) {
-			console.log(topic, partition, m.offset, m.message.value.toString('utf8'));
-			if(topic=="guru")
-			{
-				io.emit('message', {x:(new Date()).getTime(), y: m.message.value.toString('utf8')});
-			}
-			else
-			{
-				io.emit('sampleMessage', {x:(new Date()).getTime(), y: m.message.value.toString('utf8')});
-			}
-		});
-	};
- 
-	return consumer.init().then(function () {
-		// Subscribe partitons 0 and 1 in a topic: 
-		var v1= consumer.subscribe('guru', [0], dataHandler);
-	//	var v2= consumer.subscribe('Sample', [0, 1], dataHandler);
-		var arr=[];
-		arr.push([v1]);
-		console.log("val:"+arr);
-		return arr;
-		
-	});
+    connectionString: 'http://localhost:9092',
+    clientId: 'no-kafka-client'
+  });
+
+  // data handler function can return a Promise 
+  var dataHandler = function (messageSet, topic, partition) {
+    messageSet.forEach(function (m) {
+      console.log(topic, partition, m.offset, m.message.value.toString('utf8'));
+      if (topic == "guru") {
+        io.emit('message', { x: (new Date()).getTime(), y: m.message.value.toString('utf8') });
+      }
+      else {
+        io.emit('sampleMessage', { x: (new Date()).getTime(), y: m.message.value.toString('utf8') });
+      }
+    });
+  };
+
+  return consumer.init().then(function () {
+    // Subscribe partitons 0 and 1 in a topic: 
+    var v1 = consumer.subscribe('guru', [0], dataHandler);
+    //	var v2= consumer.subscribe('Sample', [0, 1], dataHandler);
+    var arr = [];
+    arr.push([v1]);
+    console.log("val:" + arr);
+    return arr;
+
+  });
 });
