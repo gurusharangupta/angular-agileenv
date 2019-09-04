ba2dfc88eb6d8d3cfbb1e805ff8b9ae24c8575a2
gurusharangupta <gupta.gurusharan@gmail.com>
2019-09-04 12:02:23 +0530

    3 files modified

--- src/app/chatroom/chatroom.component.ts
+++ src/app/chatroom/chatroom.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { WebsocketService } from '../service/websocket.service';
+import { Notification } from '../model/notification.model';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-chat-room',
@@ -8,13 +10,25 @@ import { WebsocketService } from '../service/websocket.service';
 })
 export class ChatroomComponent implements OnInit {
 
-  constructor(private websocketService: WebsocketService) { }
+  chatMessages: Notification[] = [];
+  message: string;
+  currentUser: string;
+
+  constructor(private websocketService: WebsocketService, private authService: AuthService) { }
 
   ngOnInit() {
     this.websocketService.connectToChatroom().subscribe(
       data => {
-        
+        console.log(data.message);
+
+        this.chatMessages.push(data.message);
       }
     );
+    this.currentUser = this.authService.user.getValue().email;
   }
+
+  sendMessage() {
+    this.websocketService.sendMessage(this.message);
+  }
+
 }
--- src/app/service/websocket.service.ts
+++ src/app/service/websocket.service.ts
@@ -2,6 +2,7 @@ import { Injectable, OnInit } from '@angular/core';
 
 import { Observable } from 'rxjs';
 import { Notification } from '../model/notification.model';
+import { AuthService } from '../service/auth.service';
 import * as io from 'socket.io-client';
 
 
@@ -12,19 +13,24 @@ import * as io from 'socket.io-client';
 export class WebsocketService implements OnInit {
   private url = 'http://localhost:8091';
   private socket;
-
   ngOnInit() {
 
   }
 
-  sendMessage(message) {
-    this.socket.emit('add-message', message);
+  constructor(private authService: AuthService) {
+ this.socket = io.connect(this.url);
+ 
+  }
+
+  sendMessage(msg) {
+   const email = this.authService.user.getValue().email;
+    this.socket.emit('chat', { name: email, message: msg });
     console.log("MESSAGE SENT");
   }
 
   getNotifications() {
     let observable = new Observable(observer => {
-      this.socket = io.connect(this.url);
+       //   this.socket = io.connect(this.url);
       this.socket.on('history-notifications', (data) => {
         observer.next(data);
       });
@@ -40,8 +46,9 @@ export class WebsocketService implements OnInit {
 
   connectToChatroom() {
     let observable = new Observable(observer => {
-      this.socket = io.connect(this.url);
-      this.socket.on('chatroom', (data) => {
+     //  this.socket = io.connect(this.url);
+
+      this.socket.on('chat', (data) => {
         observer.next(data);
       });
       return () => {
@@ -50,4 +57,6 @@ export class WebsocketService implements OnInit {
     });
     return observable;
   }
+
+
 }
