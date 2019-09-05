a8fc52c0672aef0ad051f48fed95b4c8575dc9ed
gurusharangupta <gupta.gurusharan@gmail.com>
2019-09-05 09:53:14 +0530

    2 files modified

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -11,26 +11,33 @@ import { take } from 'rxjs/operators';
 export class HeaderComponent implements OnInit {
 
   userLoggedIn = false;
+  userId;
 
   constructor(private authService: AuthService, private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit() {
-    
+
 
     this.authService.user.subscribe(
       user => {
-        if (!!user) { this.userLoggedIn = true; }
-        else { this.userLoggedIn = false }
+        if (!!user) {
+        this.userLoggedIn = true;
+          this.userId = user.email;
+        }
+        else {
+          this.userLoggedIn = false;
+          this.userId = null;
+        }
 
       });
 
   }
 
   logout() {
-
+    this.userId = null;
     this.authService.logout();
     this.router.navigate(['/auth']);
-    
+
 
   }
 }
